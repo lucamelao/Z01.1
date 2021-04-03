32730890ba95c9c2f3229ef9b0e5717828047002
matkwon <62735876+matkwon@users.noreply.github.com>
2021-04-03 19:59:53 -0300

    Revert "operacao xor"

--- Projetos/C-UnidadeLogicaAritmetica/src/ALU.vhd
+++ Projetos/C-UnidadeLogicaAritmetica/src/ALU.vhd
@@ -33,11 +33,10 @@ entity ALU is
 			nx:    in STD_LOGIC;                     -- inverte a entrada x
 			zy:    in STD_LOGIC;                     -- zera a entrada y
 			ny:    in STD_LOGIC;                     -- inverte a entrada y
-			f:     in STD_LOGIC_VECTOR(1 downto 0);  -- se 0 calcula x & y, se 1, x + y, resto x xor y
+			f:     in STD_LOGIC;  					 -- se 0 calcula x & y, senão x + y
 			no:    in STD_LOGIC;                     -- inverte o valor da saída
 			zr:    out STD_LOGIC;                    -- setado se saída igual a zero
 			ng:    out STD_LOGIC;                    -- setado se saída é negativa
-			carry: out STD_LOGIC;                    -- setado se saída é negativa
 			saida: out STD_LOGIC_VECTOR(15 downto 0) -- saída de dados da ALU
 	);
 end entity;
@@ -85,40 +84,27 @@ architecture  rtl OF alu is
     );
 	end component;
 
-	component Mux4Way16 is
+	component Mux16 is
 		port ( 
-				a:   in  STD_LOGIC_VECTOR(15 downto 0);
-				b:   in  STD_LOGIC_VECTOR(15 downto 0);
-				c:   in  STD_LOGIC_VECTOR(15 downto 0);
-				d:   in  STD_LOGIC_VECTOR(15 downto 0);
-				sel: in  STD_LOGIC_VECTOR(1 downto 0);
-				q:   out STD_LOGIC_VECTOR(15 downto 0));
-	end component;
-
-	component Xor16 is
-		port (
 			a:   in  STD_LOGIC_VECTOR(15 downto 0);
 			b:   in  STD_LOGIC_VECTOR(15 downto 0);
-			q:   out STD_LOGIC_VECTOR(15 downto 0)
-		);
+			sel: in  STD_LOGIC;
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
 	end component;
 
-   SIGNAL zxout,zyout,nxout,nyout,andout,adderout,xorout,muxout,precomp: std_logic_vector(15 downto 0);
+   SIGNAL zxout,zyout,nxout,nyout,andout,adderout,muxout,precomp: std_logic_vector(15 downto 0);
 
 begin
 
-	zeradorX:      zerador16 port map (zx,x,zxout);
-	inversorX:     inversor16 port map (nx,zxout,nxout);
-	zeradorY:      zerador16 port map (zy,y,zyout);
-	inversorY:     inversor16 port map (ny,zyout,nyout);
-	and:           And16 port map (nxout, nyout, andout);
-	add:           Add16 port map (nxout, nyout, adderout);
-	xor:           Xor16 port map (nxout, nyout, xorout);
-	mux:           Mux4Way16 port map (andout,adderout, xorout, xorout, f, muxout);
-	inversorFinal: inversor16 port map (no, muxout, precomp);
-	comparador:    Comparador16 port map (precomp,zr,ng);
-	
+	u0 : zerador16 port map (zx,x,zxout);
+	u1 : inversor16 port map (nx,zxout,nxout);
+	u2 : zerador16 port map (zy,y,zyout);
+	u3 : inversor16 port map (ny,zyout,nyout);
+	u4 : And16 port map (nxout, nyout, andout);
+	u5 : Add16 port map (nxout, nyout, adderout);
+	u6 : Mux16 port map (andout,adderout, f, muxout);
+	u7 : inversor16 port map (no, muxout, precomp);
+	u8 : Comparador16 port map (precomp,zr,ng);
 	saida <= precomp;
-
 
 end architecture;
