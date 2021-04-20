d61ba490f952d63ec7f9fe5d56d57c46fecabfa1
pauloschade <paulosc4@al.insper.edu.br>
2021-04-20 19:25:22 -0300

    flip flop JK

--- Projetos/D-LogicaSequencial/src/conceitoA/FlipFlopJK.vhd
+++ Projetos/D-LogicaSequencial/src/conceitoA/FlipFlopJK.vhd
@@ -18,4 +18,31 @@ architecture arch of FlipFlopJK is
 
 begin
 
+	process(clock) begin
+	if (rising_edge(CLOCK)) then
+		if (J = '1') then
+			if (K = '0') then
+				q <= '1';
+				notq <= '0';
+			elsif (K = '1') then
+				q <= notq;
+				notq <= q;
+			end if;
+		elsif (J = '0') then
+			if (K = '1') then
+				q <= '0';
+				notq <= '1';
+			elsif (K = '0') then
+				q <= q;
+				notq <= notq;
+			end if;
+		end if;
+	elsif (not rising_edge(CLOCK)) then
+		q <= q;
+		notq <= notq;
+	end if;
+	end process;
+
+
+
 end architecture;
