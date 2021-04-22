4189d048691bb5c98c4c65212c21399a50f2e908
matkwon <kwonmat@outlook.com.br>
2021-04-22 17:32:27 -0300

    tbTFF

--- Projetos/D-LogicaSequencial/testes/tb_FlipFlopT.vhd
+++ Projetos/D-LogicaSequencial/testes/tb_FlipFlopT.vhd
@@ -35,8 +35,35 @@ begin
   begin
     test_runner_setup(runner, runner_cfg);
 
-    -- IMPLEMENTE AQUI!
+		-- Teste: 0
+		t <= '0';
     wait until clk'event and clk='0';
+		assert(q = '0' and notq = '1')  report "Falha em teste: 0" severity error;
+
+		-- Teste: 1
+		t <= '1';
+    wait until clk'event and clk='0';
+		assert(q = '1' and notq = '0')  report "Falha em teste: 1" severity error;
+
+		-- Teste: 2
+		t <= '1';
+    wait until clk'event and clk='0';
+		assert(q = '0' and notq = '1')  report "Falha em teste: 2" severity error;
+
+		-- Teste: 3
+		t <= '0';
+    wait until clk'event and clk='0';
+		assert(q = '0' and notq = '1')  report "Falha em teste: 3" severity error;
+
+		-- Teste: 4
+		t <= '1';
+    wait until clk'event and clk='0';
+		assert(q = '1' and notq = '0')  report "Falha em teste: 4" severity error;
+
+		-- Teste: 5
+		t <= '0';
+    wait until clk'event and clk='0';
+		assert(q = '1' and notq = '0')  report "Falha em teste: 5" severity error;
 
     -- finish
     wait until clk'event and clk='0';
